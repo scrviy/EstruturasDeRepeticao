70976afda58cb0ecd992e030ed902c8b85b12378
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-08 14:14:00 -0300

    add loop for

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstruturasDeRepeticao
+{
+    public class EstruturasRepeticao 
+    {
+        public static void Main(string[] args)
+        {
+            int numero = 2;
+
+            for(int contador = 0; contador <= 10; contador++)
+            {
+                Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+            }
+        }
+    }
+}
